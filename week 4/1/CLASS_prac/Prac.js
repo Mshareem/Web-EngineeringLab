d65a43d6454f9edb0f0c68feda1b03180b8453d0
Shareem121 <157849437+Shareem121@users.noreply.github.com>
2024-03-05 14:35:34 +0500

    Delete week 4 directory

--- week 4/1/CLASS_prac/Prac.js
+++
@@ -1,8 +0,0 @@
-var age = 18;
-if (age >= 18) {
-console.log("You are an adult.");
-} else if (age >= 13 && age < 18) {
-console.log("You are a teenager.");
-} else {
-console.log("You are a child.");
-}
